2389739bb65eb0bd67a40e3b40b6b41b5568b98e
acrodriguezs1 <ac.rodriguezs1@uniandes.edu.co>
2025-09-15 10:50:09 -0500

    base general main

---
+++ app/src/main/java/com/example/here4u/home.kt
@@ -0,0 +1,14 @@
+package com.example.here4u
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)}
+    }
