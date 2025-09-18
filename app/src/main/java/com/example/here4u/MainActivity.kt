ea808415ed4bd21a28a35f1ec34314f7a7538131
juanrs10 <jm.ramirezs1@uniandes.edu.co>
2025-09-18 10:16:45 -0500

    Aux branch creation with UI impl

--- app/src/main/java/com/example/here4u/MainActivity.kt
+++ app/src/main/java/com/example/here4u/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.here4u
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // This links to your XML layout
+        setContentView(R.layout.activity_main)
 
-        val loginButton = findViewById<Button>(R.id.loginButton) // Finds the button by its ID
-
-        loginButton.setOnClickListener {
-            val intent = Intent(this, home::class.java) // Creates an Intent to go to 'home' Activity
-            startActivity(intent) // Starts the 'home' Activity
-        }
+        // Load ExercisesFragment when the app starts
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, ExercisesFragment())
+            .commit()
     }
 }
