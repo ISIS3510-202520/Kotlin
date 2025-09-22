c6f87ef99ea9b394b692f885d4c0850cdccf78a7
juanrs10 <jm.ramirezs1@uniandes.edu.co>
2025-09-22 17:39:59 -0500

    Login relocate

--- app/src/main/java/com/example/here4u/view/login/MainActivity.kt
+++ app/src/main/java/com/example/here4u/view/login/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.example.here4u
+package com.example.here4u.view.login
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.here4u.R
 import com.example.here4u.view.home.home
 
 class MainActivity : AppCompatActivity() {
