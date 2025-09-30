a68719614489efc5f14336fad6f2e1a161359fd2
Sara Rozo <s.rozoo@uniandes.edu.co>
2025-09-30 00:41:54 -0500

    Local Data Base connection

---
+++ app/src/main/java/com/example/here4u/data/local/dao/JournalDao.kt
@@ -0,0 +1,47 @@
+package com.example.here4u.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.here4u.data.local.entity.JournalEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface JournalDao {
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(entry: JournalEntity): Long
+
+    @Update
+    suspend fun update(entry: JournalEntity)
+
+    @Delete
+    suspend fun delete(entry: JournalEntity)
+
+    @Query("DELETE FROM Journal_table WHERE id = :id")
+    suspend fun deleteById(id: Int)
+
+    @Query("""
+    SELECT * FROM Journal_table
+    WHERE date >= :fromInclusive
+    ORDER BY date DESC
+    LIMIT :limit
+""")
+    fun getSince(
+        fromInclusive: Long,
+        limit: Long = 100
+    ): Flow<List<JournalEntity>>
+
+    @Query("""
+    SELECT * FROM Journal_table
+    WHERE emotionId = :emotionId
+    ORDER BY date DESC
+""")
+    fun getForEmotion(emotionId: Long): Flow<List<JournalEntity>>
+
+}
--- app/src/main/java/com/example/here4u/view/emotions/IdentifyingEmotions.kt
+++ app/src/main/java/com/example/here4u/view/emotions/IdentifyingEmotions.kt
@@ -2,55 +2,46 @@ package com.example.here4u.view.emotions
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.here4u.databinding.ActivityIdentifyingEmotionsBinding
-import com.example.here4u.model.Emotion
+import com.example.here4u.data.local.entity.EmotionEntity
 import com.example.here4u.view.journaling.Journaling
+import com.example.here4u.viewmodel.EmotionsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class IdentifyingEmotions : AppCompatActivity() {
-    val C_PEACH = "#FFDBD2".toColorInt()
-    val C_TEAL  = "#8CC0CF".toColorInt()
-    val C_SKY   = "#86D9F0".toColorInt()
-    val C_SLATE = "#7C8FBB".toColorInt()
 
-
-    val emotions = listOf(
-        // JOY (peach)
-        Emotion("Serenity", C_PEACH),
-        Emotion("Joy", C_PEACH),
-        Emotion("Ecstasy", C_PEACH),
-
-        // TRUST (teal)
-        Emotion("Acceptance", C_TEAL),
-        Emotion("Trust", C_TEAL),
-        Emotion("Admiration", C_TEAL),
-
-        // SURPRISE (sky)
-        Emotion("Distraction", C_SKY),
-        Emotion("Surprise", C_SKY),
-        Emotion("Amazement", C_SKY),
-
-        // SADNESS (slate)
-        Emotion("Pensiveness", C_SLATE),
-        Emotion("Sadness", C_SLATE),
-        Emotion("Grief", C_SLATE)
-    )
+    private val viewModel: EmotionsViewModel by viewModels()
     private lateinit var binding: ActivityIdentifyingEmotionsBinding
+    private lateinit var adapter: EmotionsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityIdentifyingEmotionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.rvEmotions.adapter = EmotionsAdapter(emotions) { element ->
-            val intent = Intent(this, Journaling::class.java)
-            intent.putExtra("emotion_name", element.name)
-            intent.putExtra("emotion_color", element.color)
+
+        adapter = EmotionsAdapter { emotion->
+            val intent = Intent(this@IdentifyingEmotions, Journaling::class.java).apply {
+                putExtra("emotion_name", emotion.name)
+                putExtra("emotion_color", emotion.color)
+                putExtra("emotion_description", emotion.description)
+            }
             startActivity(intent)
         }
+
         binding.rvEmotions.layoutManager = GridLayoutManager(this, 2)
+        binding.rvEmotions.adapter = adapter
+
+        viewModel.emotions.observe(this){
+            list-> adapter.updateData(list)
+        }
+
+
 
 
     }
