055de065d573d8ed84534e8969585f3e3daf7807
juanrs10 <jm.ramirezs1@uniandes.edu.co>
2025-10-03 19:18:49 -0500

    "Import business logic modules from Summaries-view-#10"

---
+++ app/src/main/java/com/example/here4u/model/Emotion.kt
@@ -0,0 +1,11 @@
+package com.example.here4u.model
+
+import androidx.annotation.ColorInt
+
+data class Emotion(
+    val id: Long,
+    val name: String,
+    val color: Int,
+    val description: String
+    // val score: Float
+)
