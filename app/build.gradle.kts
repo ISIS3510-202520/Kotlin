3bebbdde706755185ff8dc12d502d479fc18f06a
Juan Manuel Ramírez <110953461+juanrs10@users.noreply.github.com>
2025-11-01 02:48:32 -0500

    Bundle event creation for interactions with emergency contacts (#81)
    Perfect

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,6 +139,9 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.messaging)
 
+
+
+    // OpenAI API / Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
