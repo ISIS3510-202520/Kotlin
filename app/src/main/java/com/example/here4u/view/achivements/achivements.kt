7a95e72e1c4603ff03eef8d8ffb517fa046d9706
acrodriguezs1 <123606873+acrodriguezs1@users.noreply.github.com>
2025-11-29 00:09:25 -0500

    Feature/achivementsview#96 (#104)
    * microoptimization findfindViewById for binding
    
    * changes in achivements viwe
    
    * changes in achivements viwe
    
    * cambios en el achivements

---
+++ app/src/main/java/com/example/here4u/view/achivements/achivements.kt
@@ -0,0 +1,15 @@
+package com.example.here4u.view.achivements
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.here4u.R
+
+// achivements changesqqq
+class achivements : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_achivements)
+
+    }
+}
